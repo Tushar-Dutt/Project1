0cb294e3c878a6f2cbb54d53fc15df39c3683e1d
Tushar-Dutt <tushar.dutt@newvisionsoftware.io>
2022-08-10 12:43:43 +0530

    First commit

---
+++ src/test/java/com/test/projectjenkins/ProjectJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.projectjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
