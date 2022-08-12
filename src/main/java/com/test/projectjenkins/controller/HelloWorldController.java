b8619c3ce85c71b8d0ff47035f6798ea3cceba29
Tushar-Dutt <tushar.dutt@newvisionsoftware.io>
2022-08-12 16:01:17 +0530

    Adding message in different style

--- src/main/java/com/test/projectjenkins/controller/HelloWorldController.java
+++ src/main/java/com/test/projectjenkins/controller/HelloWorldController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public ResponseEntity<String> get() {
-        return ResponseEntity.ok("Hello World Hii!");
+        String str = "\n" + "lets run and display changes";
+        return ResponseEntity.ok("Hello World Hii!"+str);
     }
 }
